Preview images, fix rotation, mapping of characters

data/alphabets/emnist/extract.py
@@ -23,6 +23,9 @@ def extract(nb_images=3) -> Tuple[Dict[int, np.ndarray], np.ndarray]:
     """
     dataset = loadmatfile()['dataset']
     training, testing, mapping = dataset[0][0]
+    characters = np.vectorize(chr)(mapping[:, -1])
+    mapping = np.hstack((mapping, characters.reshape(-1, 1)), dtype=object)
+
     training, testing = training[0], testing[0]
     
     print('Extracting labels')
@@ -44,5 +47,6 @@ def extract(nb_images=3) -> Tuple[Dict[int, np.ndarray], np.ndarray]:
     print(images.shape)
     chosen_images = {}
     for label, positions in chosen_positions.items():
-        chosen_images[label] = images[positions].reshape(-1, 28, 28)
+        chosen_images[label] = np.transpose(
+            images[positions].reshape(-1, 28, 28), (0, 2, 1))
     return chosen_images, mapping

data/alphabets/kmnist/extract.py
@@ -32,7 +32,7 @@ def extract(nb_images=3, nb_remove=1) -> Tuple[Dict[int, np.ndarray], np.ndarray
     """
     print('Extracting labels')
     labels = np.array([], dtype=int)
-    for file in FILES[1::2]:
+    for file in [FILES[1]]:
         print('Extracting {}'.format(os.path.basename(file)))
         labels = np.append(labels, loadnpzfile(file))
     
@@ -55,7 +55,7 @@ def extract(nb_images=3, nb_remove=1) -> Tuple[Dict[int, np.ndarray], np.ndarray
     
     print('Extracting images')
     images = None
-    for file in FILES[0:4:2]:
+    for file in [FILES[0]]:
         print('Extracting {}'.format(os.path.basename(file)))
         images = loadnpzfile(file) if images is None \
             else np.concatenate((images, loadnpzfile(file)))
@@ -66,3 +66,5 @@ def extract(nb_images=3, nb_remove=1) -> Tuple[Dict[int, np.ndarray], np.ndarray
     
     mapping = loadcsvfile(FILES[-1]).to_numpy()
     return chosen_images, mapping
+
+extract()